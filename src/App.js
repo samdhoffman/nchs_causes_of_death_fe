7b46352f033409375d6efdd76e50201184b4817b
Sam Hoffman <samdhoffman@gmail.com>
2020-03-20 22:59:59 -0400

    Fetch initial data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  // Cause of death data set from api
+  const [causeOfDeathData, setCauseOfDeathData] = useState({});
+
+  // Loading & Errors
+  const [isLoading, setIsLoading] = useState(false); // used for loading indicator
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    fetchData(); // Get data on page load
+  }, []);
+
+  // Use this method to load our data from the api
+  const fetchData = async () => {
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const result = await axios.get("/causes-of-death"); // get data from our api
+      setCauseOfDeathData(result.data);
+    } catch (error) {
+      setIsError(true);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
     <div className="App">
       
