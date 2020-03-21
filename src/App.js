5473bb1994d8a005b846834b39e213d6e68ebdcd
Sam Hoffman <samdhoffman@gmail.com>
2020-03-21 11:30:10 -0400

    Add loading spinner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 function App() {
   // Cause of death data set from api
   const [causeOfDeathData, setCauseOfDeathData] = useState({});
@@ -21,17 +23,25 @@ function App() {
 
     try {
       const result = await axios.get("/causes-of-death"); // get data from our api
-      setCauseOfDeathData(result.data);
+      setTimeout(() => { // Using set timeout to allow the display of the loading indicator to give feedback to the user
+        setCauseOfDeathData(result.data);
+        setIsLoading(false);
+      }, 1000)
     } catch (error) {
       setIsError(true);
+      setIsLoading(false);
     }
-
-    setIsLoading(false);
   };
 
   return (
     <div className="App">
-      
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <h1>hi</h1>
+      )}
     </div>
   );
 }
